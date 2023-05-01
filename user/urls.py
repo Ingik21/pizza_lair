74acd70e813f88dea4c63fe72d307f4419d670b6
Ingi Kr <ingik21@ru.is>
2023-05-01 13:21:55 +0000

    cmt

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index,name="manufacturer-index"),
+    path('', views.index,name="user-index"),
 ]
--- user/views.py
+++ user/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'manufacturer/index.html')
+    return render(request, 'user/index.html')
