cd4028b1f390e88c8810b9305a3f740159caf652
Bjarturblaer <70706679+Bjarturblaer@users.noreply.github.com>
2023-05-02 16:48:08 +0000

    search bar

---
+++ static/js/pizzas.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('#search-btn').on('click', function (e) {
+        e.preventDefault();
+        var searchText = $('#search-box').val();
+        $.ajax({
+            url: '/pizzas?search_filter=' + searchText,
+            type: 'GET',
+            success: function (resp) {
+                var newHtml = resp.data.map(d => {
+                    return `<div class="wellPizza">
+                                <a href="/pizzas/${d.id}">
+                                    <img class="pizzaImage" src="${d.firstImage}" alt=""/>
+                                    <h4>${d.name}</h4>
+                                    <p>${d.description}</p>
+                                </a>
+                            </div>`
+                });
+                $('.pizzas').html(newHtml.join(''));
+                $('#search-box').val('');
+        },
+        error: function (xhr, status, error) {
+                //TODO: Show toastr error
+                console.error(error);
+            }
+        });
+    });
+});
